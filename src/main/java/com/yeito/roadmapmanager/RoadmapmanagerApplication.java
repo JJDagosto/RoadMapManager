0bae040534283e9880ff95579f4fd8ce3e162187
Yeito <juanjodagosto@gmail.com>
2025-05-19 00:28:29 -0300

    Initial commit

---
+++ src/main/java/com/yeito/roadmapmanager/RoadmapmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.yeito.roadmapmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmapmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadmapmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeito/roadmapmanager/RoadmapmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeito.roadmapmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
